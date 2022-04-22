b1d4e9e8ea0a9c9d65a3be361d15ce383189e093
dolly.ograce <dolly.ograce@gmail.com>
2022-04-22 17:18:51 -0400

    Updated goal page

--- src/pages/Goal.js
+++ src/pages/Goal.js
@@ -7,48 +7,46 @@ import SchoolIcon from '@mui/icons-material/School';
 import WorkIcon from '@mui/icons-material/Work';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import logo from '../components/Spyglass-logos_transparent.png';
+import { useNavigate } from 'react-router-dom';
 
 
 
 export default function FloatingActionButtonExtendedSize() {
+
+
+  const navigate = useNavigate();
   return (
-    <><img src={logo} height="250px" className="logo" alt="" ></img><><h1>Welcome, Username!
+    <><img src={logo} height="250px" className="logo" alt=""></img><><h1>Welcome, Username!
     </h1>
       <h2>What are your current savings goals?</h2>
 
       <Box sx={{ '& > :not(style)': { m: 1 } }}>
-
         <div className='col-12'>
-          <Fab variant="extended" color="primary" aria-label="add">
+          <Fab variant="extended" color="primary" aria-label="add" onClick={() => navigate("/create")}>
             <BeachAccessIcon sx={{ mr: 1 }} />
             Vacation
           </Fab>
         </div>
-
-
         <div className='col-12'>
-          <Fab variant="extended" color="primary" aria-label="add">
+          <Fab variant="extended" color="primary" aria-label="add" onClick={() => navigate("/create")}>
             <WorkIcon sx={{ mr: 1 }} />
             Business
           </Fab>
         </div>
-
         <div className='col-12'>
-          <Fab variant="extended" color="primary" aria-label="add">
+          <Fab variant="extended" color="primary" aria-label="add" onClick={() => navigate("/create")}>
             <PersonIcon sx={{ mr: 1 }} />
             Personal
           </Fab>
         </div>
-
         <div className='col-12'>
-          <Fab variant="extended" color="primary" aria-label="add">
+          <Fab variant="extended" color="primary" aria-label="add" onClick={() => navigate("/create")}>
             <SchoolIcon sx={{ mr: 1 }} />
             Education
           </Fab>
         </div>
-
         <div className='col-12'>
-          <Fab variant="extended" color="primary" aria-label="add">
+          <Fab variant="extended" color="primary" aria-label="add" onClick={() => navigate("/create")}>
             <LightbulbIcon sx={{ mr: 1 }} />
             Custom Goal
           </Fab>
@@ -56,5 +54,3 @@ export default function FloatingActionButtonExtendedSize() {
       </Box></></>
   );
 }
-
-
